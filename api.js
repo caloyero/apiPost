c1878d2ebe37c8aaa1e3d3757b43b3bdcc0f51cb
omar perlaza <caliyero@gmail.com>
2022-11-10 15:26:42 -0500

    api.post 0.0.2

--- api.js
+++ api.js
@@ -22,8 +22,11 @@ connection.connect(function(error){
     }
 })
 
+/* posts */
+
+/* listar post */
 api.get('/api/post',(request,response)=>{
-    connection.query('SELECT * FROM post', (error,filas)=>{
+    connection.query('SELECT * FROM usuarios us,post ps WHERE us.id = ps.usuarios_id', (error,filas)=>{
         if (error) {
             throw error
         }else{
@@ -32,9 +35,9 @@ api.get('/api/post',(request,response)=>{
     })
 })
 
-
-api.get('/api/post/:id',(request,response)=>{
-    connection.query('SELECT * FROM post WHERE id = ?',[request.params.id], (error,fila)=>{
+/* listar post por id */
+api.get('/api/post/perfil/:id',(request,response)=>{
+    connection.query('SELECT ps.id,ps.titulo,ps.contenido,ps.usuarios_id,ps.likes,ps.imagen,us.foto_de_perfil,us.nombre FROM usuarios us,post ps WHERE ps.usuarios_id = us.id and us.id = ?',[request.params.id], (error,fila)=>{
         if (error) {
             throw error
         }else{
@@ -43,6 +46,7 @@ api.get('/api/post/:id',(request,response)=>{
     })
 })
 
+/* crear post  */
 api.post('/api/post', (request,response)=>{
     let data ={
         titulo:request.body.titulo,
@@ -61,6 +65,44 @@ api.post('/api/post', (request,response)=>{
     });
 });
 
+/* actuaslizar post */
+
+api.put('/api/post/:id',(request,response)=>{
+    let id = request.params.id;
+    let titulo = request.body.titulo;
+    let contenido = request.body.contenido;
+    let usuarios_id = request.body.usuarios_id;
+    let sql = "UPDATE posT SET titulo = ?, contenido = ?, usuarios_id = ? WHERE id = ?";
+
+    connection.query(sql, [titulo, contenido, usuarios_id, id], function(error, result)
+    {
+         if (error) {
+            throw error
+        } else{
+            response.send(result);
+        }
+    });
+    
+});
+
+/* dar likes */
+
+api.put('/api/post/likes/:id',(request,response)=>{
+    let id = request.params.id;
+    let likes = request.body.likes;
+    let sql = "UPDATE posT SET likes = ? WHERE id = ?";
+
+    connection.query(sql, [likes, id], function(error, result)
+    {
+         if (error) {
+            throw error
+        } else{
+            response.send(result);
+        }
+    });
+    
+});
+
 
 // usuario
 
@@ -74,9 +116,33 @@ api.get('/api/user/:id',(request,response)=>{
         }
     })
 });
+// foto de  perfil
+api.get('/api/userPerfil/:id',(request,response)=>{
+    connection.query('SELECT foto_de_perfil FROM usuarios WHERE id = ?',[request.params.id],(error,fila)=>{
+        if(error)
+        {
+            throw error;
+        }else{
+            response.send(fila);
+        }
+    })
+});
 
 api.get('/api/postFromUserId/:id',(request,response)=>{
     connection.query('SELECT * FROM post WHERE usuarios_id = ?',[request.params.id],(error, fila)=>{
+        if(error)
+        {
+            throw error;
+        }else{
+            response.send(fila);
+        }
+    })
+})
+
+/* comentarios */
+
+api.get('/api/comentariosFromPostId/:id',(request,response)=>{
+    connection.query('SELECT * FROM comentarios WHERE post_id = ?',[request.params.id],(error, fila)=>{
         if(error)
         {
             throw error;
